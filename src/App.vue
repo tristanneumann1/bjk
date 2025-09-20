241d2a61d0faaf5d0ddb696c2f9c9418c2c092e4
tristan-neumann-wheel <122320074+tristan-neumann-wheel@users.noreply.github.com>
2025-09-20 00:12:06 -0400

    Add Table component

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,11 @@
 <script setup lang="ts">
-import PlayingCard from "@/components/PlayingCard.vue";
-import CardHand from "@/components/CardHand.vue";
-import Chair from "@/components/Chair.vue";
-import PlayerBalanceDisplay from "@/components/PlayerBalanceDisplay.vue";
-import ActionSection from "@/components/ActionSection.vue";
-import DealerSection from "@/components/DealerSection.vue";
-
-const HAND_SPADES = [{ value: 'A', suit: 'spades' },
-    { value: '2', suit: 'spades' },
-    { value: '3', suit: 'spades' },
-    { value: '4', suit: 'spades' },
-    { value: '5', suit: 'spades' },
-    { value: '6', suit: 'spades' },
-    { value: '7', suit: 'spades' },
-    { value: '8', suit: 'spades' },
-    { value: '9', suit: 'spades' },
-    { value: '10', suit: 'spades' }]
-
-const HAND_HEARTS = [{ value: 'A', suit: 'hearts' },
-    { value: '2', suit: 'hearts' },
-    { value: '3', suit: 'hearts' },
-    { value: '4', suit: 'hearts' },
-    { value: '5', suit: 'hearts' },
-    { value: '6', suit: 'hearts' },
-    { value: '7', suit: 'hearts' },
-    { value: '8', suit: 'hearts' },
-    { value: '9', suit: 'hearts' },
-    { value: '10', suit: 'hearts' }]
-
-
-const HAND_KINGS = [{ value: 'K', suit: 'hearts' },
-    { value: 'K', suit: 'spades' },
-  { value: 'K', suit: 'clubs' },
-  { value: 'K', suit: 'diamonds' },]
-
-const HAND_BLACKJACK = [{ value: 'K', suit: 'hearts' },
-    { value: 'A', suit: 'spades' }]
-
-const HAND_STRAIGHT = [
-    { value: '3', suit: 'hearts' },
-    { value: '4', suit: 'diamonds' },
-    { value: '5', suit: 'hearts' },
-    { value: '6', suit: 'clubs' },
-    { value: '7', suit: 'hearts' }]
+import Table from "@/components/Table.vue";
 </script>
 
 <template>
 
 
-  <div :style="{
-    width: '100%',
-    display: 'flex',
-    gap: '2rem',
-    alignItems: 'flex-end',
-    justifyContent: 'space-between',
-  }">
-    <DealerSection />
-    <ActionSection />
-  </div>
-
-  <div :style="{
-    width: '100%',
-    display: 'flex',
-    gap: '2rem',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  }">
-    <Chair :hands="[HAND_SPADES, HAND_HEARTS, HAND_KINGS, HAND_BLACKJACK, HAND_STRAIGHT]" />
-    <Chair :hands="[HAND_SPADES, HAND_HEARTS, HAND_KINGS, HAND_BLACKJACK, HAND_STRAIGHT]" />
-    <Chair :hands="[HAND_SPADES, HAND_HEARTS, HAND_KINGS, HAND_BLACKJACK, HAND_STRAIGHT]" />
-  </div>
-  <PlayerBalanceDisplay/>
+  <Table />
 </template>
 
 <style>
---
+++ src/components/Table.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Chair from "@/components/Chair.vue";
+import PlayerBalanceDisplay from "@/components/PlayerBalanceDisplay.vue";
+import ActionSection from "@/components/ActionSection.vue";
+import DealerSection from "@/components/DealerSection.vue";
+</script>
+
+<template>
+  <div class="table-upper">
+    <DealerSection />
+    <ActionSection />
+  </div>
+
+  <div class="table-lower">
+    <Chair :hands="[]" />
+    <Chair :hands="[]" />
+    <Chair :hands="[]" />
+  </div>
+  <PlayerBalanceDisplay/>
+</template>
+
+<style scoped>
+.table-upper {
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+    justify-content: space-between;
+}
+.table-lower {
+  width: 100%;
+    display: flex;
+    gap: 2rem;
+    align-items: flex-end;
+    justify-content: center;
+}
+</style>
