c51146980f22b037229f9ca51b693fe55ea49725
tristan-neumann-wheel <122320074+tristan-neumann-wheel@users.noreply.github.com>
2025-09-19 20:29:40 -0400

    Add HAnd vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <script setup lang="ts">
 import PlayingCard from "@/components/PlayingCard.vue";
 import CardHand from "@/components/CardHand.vue";
+import Hand from "@/components/Hand.vue";
+
+const HAND_SPADES = [{ value: 'A', suit: 'spades' },
+    { value: '2', suit: 'spades' },
+    { value: '3', suit: 'spades' },
+    { value: '4', suit: 'spades' },
+    { value: '5', suit: 'spades' },
+    { value: '6', suit: 'spades' },
+    { value: '7', suit: 'spades' },
+    { value: '8', suit: 'spades' },
+    { value: '9', suit: 'spades' },
+    { value: '10', suit: 'spades' }]
+
+const HAND_HEARTS = [{ value: 'A', suit: 'hearts' },
+    { value: '2', suit: 'hearts' },
+    { value: '3', suit: 'hearts' },
+    { value: '4', suit: 'hearts' },
+    { value: '5', suit: 'hearts' },
+    { value: '6', suit: 'hearts' },
+    { value: '7', suit: 'hearts' },
+    { value: '8', suit: 'hearts' },
+    { value: '9', suit: 'hearts' },
+    { value: '10', suit: 'hearts' }]
+
+
+const HAND_KINGS = [{ value: 'K', suit: 'hearts' },
+    { value: 'K', suit: 'spades' },
+  { value: 'K', suit: 'clubs' },
+  { value: 'K', suit: 'diamonds' },]
+
+const HAND_BLACKJACK = [{ value: 'K', suit: 'hearts' },
+    { value: 'A', suit: 'spades' }]
+
+const HAND_STRAIGHT = [
+    { value: '3', suit: 'hearts' },
+    { value: '4', suit: 'diamonds' },
+    { value: '5', suit: 'hearts' },
+    { value: '6', suit: 'clubs' },
+    { value: '7', suit: 'hearts' }]
 </script>
 
 <template>
@@ -81,7 +120,9 @@ import CardHand from "@/components/CardHand.vue";
     // { value: 'Q', suit: 'spades' },
     // { value: 'K', suit: 'spades' }
 
-  ]" large />
+  ]"/>
+
+  <Hand :hands="[HAND_SPADES, HAND_HEARTS, HAND_KINGS, HAND_BLACKJACK, HAND_STRAIGHT]" />
 </template>
 
 <style scoped></style>
