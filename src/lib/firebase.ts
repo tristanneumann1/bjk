00c970696be99f979c7c57e9b41442c5031ae076
TristanPersonal <tristanneumann1@gmail.com>
2026-01-08 11:40:15 -0500

    fix redirect link

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const analytics = getAnalytics(fbApp);
 const appEnv = import.meta.env.VITE_APP_ENV;
 
 const actionCodeSettings = {
-  url: appEnv === 'local' ? `http://localhost:${PORT}/bjk/finishSignUp`: 'https://www.github.io/bjk/finishSignUp',
+  url: appEnv === 'local' ? `http://localhost:${PORT}/bjk/finishSignUp`: 'https://tristanneumann1.github.io/bjk/finishSignUp',
   handleCodeInApp: true,
   // The domain must be configured in Firebase Hosting and owned by the project.
   // linkDomain: 'www.github.io'
